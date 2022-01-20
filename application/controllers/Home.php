60222683f5ce57ed4a639d7b280a7d0cb8b4aa27
kevin Gates <kevinobama@gmail.com>
2022-01-20 10:51:44 +0800

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,13 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
 
-    public function __construct()
-    {
-        $this->load->model('UserModel');
+    public function __construct() {
+        parent::__construct();
     }
 
-	public function index()
+    public function index()
 	{
 		$this->load->view('home');
+        //$this->load->view('/users/registration',compact('users'));
 	}
 }
--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -10,4 +10,29 @@ class UserModel extends CI_Model {
     {
         return "data";
     }
+
+    public function getUserLogin($email, $password)
+    {
+        $query = $this->db->get_where('users', array('email' => $email, 'password' => md5($password)));
+
+        return $query->row_array();
+    }
+
+    public function registration($id = 0)
+    {
+        $data = array(
+            'firstname' => $this->input->post('firstname'),
+            'lastname' => $this->input->post('lastname'),
+            'email' => $this->input->post('email'),
+            'password' => md5($this->input->post('password')),
+            'updated_at' => date('Y-m-d H:i:s')
+        );
+
+        if ($id == 0) {
+            return $this->db->insert('users', $data);
+        } else {
+            $this->db->where('id', $id);
+            return $this->db->update('users', $data);
+        }
+    }
 }
